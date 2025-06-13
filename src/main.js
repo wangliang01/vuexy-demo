c22b7a7f763f3753084261df2e05be12e1b4af05
EXT Mike Wang <mike.wang.ext@razer.com>
2025-06-13 16:30:27 +0800

    feat: 作测试

--- src/main.js
+++ src/main.js
@@ -1,10 +1,13 @@
-import { createApp } from 'vue'
+import '@core/scss/template/index.scss'
+import '@styles/styles.scss'
+
 import App from '@/App.vue'
+import { createApp } from 'vue'
 import { registerPlugins } from '@core/utils/plugins'
 
 // Styles
-import '@core/scss/template/index.scss'
-import '@styles/styles.scss'
+
+
 
 // Create vue app
 const app = createApp(App)
@@ -12,6 +15,9 @@ const app = createApp(App)
 
 // Register plugins
 registerPlugins(app)
+
+// 作一点测试
+console.log('测试')
 
 // Mount vue app
 app.mount('#app')
